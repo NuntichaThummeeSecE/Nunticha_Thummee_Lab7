d755b313370c5ca0c89202f4cd59c5ce9040eed0
NuntichaThummeeSecE <n01687478@humber.ca>
2024-10-15 13:22:35 -0400

    add_display_checkbox_with_user_input

--- lab7.js
+++ lab7.js
@@ -1,18 +1,19 @@
-const addNewItem = document.getElementById("addButton");
-const container = document.getElementById("listBox");
-const typeBox = document.getElementById("typeBox");
+const addNewItem = document.getElementById(`addButton`);
+const container = document.getElementById(`listBox`);
+const typeBox = document.getElementById(`typeBox`);
 
-addNewItem.addEventListener("click", addPlan);
+addNewItem.addEventListener(`click`, addPlan);
 
 function addPlan() {
     const userInput = typeBox.value;
-    let newItem = document.createElement("li");   
-    newItem.innerHTML = userInput;
+    let newItem = document.createElement(`li`);
 
-    let checkBox = document.createElement("input")
-    checkBox.type = "checkbox"; 
+    let checkBox = document.createElement(`input`)
+    checkBox.type = `checkbox`;
 
     newItem.appendChild(checkBox);
     container.appendChild(newItem);
+
+    newItem.innerHTML = `${checkBox.outerHTML} ${userInput}`;
 }
 
